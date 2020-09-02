d6cf7cedb1621d08b7a93b029e40558cbb0f98fd
duanegtr <61340105+duanegtr@users.noreply.github.com>
2020-09-02 12:30:47 -0500

    Update clashroyaleclans.py

--- clashroyaleclans/clashroyaleclans.py
+++ clashroyaleclans/clashroyaleclans.py
@@ -103,7 +103,7 @@ class ClashRoyaleClans(commands.Cog):
         self.bot.loop.create_task(self.clash.close())
 
 
-    @commands.command(name="legend")
+    @commands.command(name="tl")
     async def commandLegend(
         self,
         ctx: commands.Context,
@@ -111,7 +111,7 @@ class ClashRoyaleClans(commands.Cog):
         account: int = 1,
     ):
         """
-        Show Legend clans.
+        Show TL clans.
         Can also show clans based on a member's stats
         """
         async with ctx.channel.typing():
@@ -160,8 +160,8 @@ class ClashRoyaleClans(commands.Cog):
 
             embed = discord.Embed(color=0xFAA61A)
             embed.set_author(
-                name="Legend Family Clans",
-                url="http://royaleapi.com/clan/family/legend",
+                name="Threat Level Family Clans",
+                url="http://royaleapi.com/clan/family/threatlevel",
                 icon_url="https://cdn.discordapp.com/attachments/423094817371848716/425389610223271956/legend_logo-trans.png",
             )
 
