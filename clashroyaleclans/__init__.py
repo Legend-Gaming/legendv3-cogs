e31a2ac67706369c9e48c004f4871e0f950236f1
Suven-p <pandeysuven@gmail.com>
2020-10-24 11:22:26 +0545

    New clashroyaleclans

--- clashroyaleclans/__init__.py
+++ clashroyaleclans/__init__.py
@@ -1,8 +1,13 @@
+import json
+from pathlib import Path
+
+from redbot.core.bot import Red
+
 from .clashroyaleclans import ClashRoyaleClans
 
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-async def setup(bot):
-    cog = ClashRoyaleClans(bot=bot)
-    # await cog.crtoken()
-    # await cog.refresh_data()
-    bot.add_cog(cog)
+
+async def setup(bot: Red) -> None:
+    bot.add_cog(ClashRoyaleClans(bot))
