0a15d689c650c5dfceae053367de3d7191eb2f41
Suven Pandey <pandeysuven@gmail.com>
2020-10-30 18:13:38 +0545

    Fix cog name change

--- clashroyaleclansv2/__init__.py
+++ clashroyaleclansv2/__init__.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .clashroyaleclans import ClashRoyaleClans
+from .clashroyaleclans import ClashRoyaleClans2
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red) -> None:
-    bot.add_cog(ClashRoyaleClans(bot))
+    bot.add_cog(ClashRoyaleClans2(bot))
