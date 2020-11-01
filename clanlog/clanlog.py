8dd56c8a5756617f9298675f9dcfa883f2426ec9
Suven Pandey <pandeysuven@gmail.com>
2020-11-01 10:28:11 +0545

    New version of clanlog

--- clanlog/clanlog.py
+++ clanlog/clanlog.py
@@ -1,24 +1,11 @@
-import asyncio
-import json
 import logging
-from copy import deepcopy
-from datetime import datetime
-from time import time, sleep
 from typing import Optional
 
-import clashroyale
 import discord
-from discord.ext import tasks
 from redbot.core import Config, checks, commands
-from redbot.core.commands.commands import command
-from redbot.core.data_manager import cog_data_path
-from redbot.core.utils import AsyncIter
+
 
 log = logging.getLogger("red.cogs.clanlog")
-
-
-class NoToken(Exception):
-    pass
 
 
 class NoClansCog(Exception):
@@ -31,214 +18,90 @@ class ClanLog(commands.Cog):
 
         self.config = Config.get_conf(self, identifier=6942053)
         default_global = {
-            "log_channel": 747520169404006483,
-            "debug_channel": 747520169404006483,
-            "clans": list(),
+            "global_log_channel": None,
         }
         self.config.register_global(**default_global)
-        sleep(2)
-        self.crclans = self.bot.get_cog("ClashRoyaleClans")
+        self.crclans = self.bot.get_cog("ClashRoyaleClans2")
         if self.crclans is None:
-            log.error("Load clashroyale clans for this cog to work.")
+            log.error("Load clashroyaleclans cog for this cog to work.")
             raise NoClansCog
-        self.claninfo_path = str(cog_data_path(self.crclans) / "clans.json")
 
-        self.refresh_task = self.clan_log.start()
-        self.token_task = self.bot.loop.create_task(self.crtoken())
-        self.clash = None
-        self.last_updated = None
-        self.last_updated_preety = None
-        self.last_error_time = None
-        self.loop_count = 0
-
-    async def crtoken(self):
-        # Initialize clashroyale API
-        token = await self.bot.get_shared_api_tokens("clashroyale")
-        if not token or token.get("token") is None:
-            log.error(
-                "CR Token is not SET. "
-                "Use [p]set api clashroyale token,YOUR_TOKEN to set it"
-            )
-            raise NoToken
-        self.clash = clashroyale.official_api.Client(
-            token=token["token"], is_async=True, url="https://proxy.royaleapi.dev/v1"
-        )
-
-    async def refresh_data(self):
-        try:
-            async with crclans.claninfo_lock:
-                with open(self.claninfo_path) as file:
-                    self.family_clans = dict(json.load(file))
-            all_clan_data = list()
-            for name, data in self.family_clans.items():
-                try:
-                    clan_tag = data["tag"]
-                    clan_data = await self.clash.get_clan(clan_tag)
-                    all_clan_data.append(
-                        {
-                            "name": clan_data["name"],
-                            "tag": clan_data["tag"],
-                            "member_list": clan_data["member_list"],
-                        }
-                    )
-                # REMINDER: Order is important. RequestError is base exception class.
-                except clashroyale.NotFoundError:
-                    log.critical("Invalid clan tag.")
-                    raise
-                except clashroyale.RequestError:
-                    log.error("Error: Cannot reach ClashRoyale Server.")
-                    raise
-            await self.config.clans.set(all_clan_data)
-            # log.info("Updated data for all clans.")
-        except Exception as e:
-            log.error("Encountered exception {} when refreshing clan data.".format(e))
-            raise
-
-    @tasks.loop(seconds=30)
-    async def clan_log(self):
-        """Check current data against data stored in config and log members joined or left in log channel."""
-
-        log_channel_id = await self.config.log_channel()
-        debug_channel_id = await self.config.debug_channel()
+    @commands.Cog.listener(name="on_clandata_update")
+    async def on_clandata_update(self, old_data, new_data):
+        log.error("Listener on")
+        log_channel_id = await self.config.global_log_channel()
         log_channel = self.bot.get_channel(log_channel_id)
-        debug_channel = self.bot.get_channel(debug_channel_id)
-
         if log_channel is None:
-            log.error("The channel to send log messages is not setup correctly.")
+            log.error("Global log channel is not setup correctly.")
             return
 
-        if debug_channel:
-            await debug_channel.send(
-                "Loop iteration {} has started at {}.".format(
-                    self.loop_count, datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                )
+        # old_data = kwargs.get("old_data")
+        # new_data = kwargs.get("new_data")
+        if len(old_data) == 0 or old_data is None:
+                log.error("Old data is " + old_data)
+                return
+        if len(new_data) == 0 or new_data is None:
+                log.error("New data is " + new_data)
+                return
+        for key, data in new_data.items():
+            old_members_data = {}
+            new_members_data = {}
+            # When a clan is added
+            if key not in old_data.keys():
+                log.error(f"Clan {key} not found in old_data.")
+                continue
+            for member_data in old_data[key]["member_list"]:
+                old_members_data[member_data["tag"]] = member_data["name"]
+            for member_data in data["member_list"]:
+                new_members_data[member_data["tag"]] = member_data["name"]
+
+            total = set(list(new_members_data.keys())).union(
+                set(list(old_members_data.keys()))
             )
+            players_left_clan = set(total - set(new_members_data.keys()))
+            players_joined_clan = set(total - set(old_members_data.keys()))
 
-        if self is not self.bot.get_cog("ClanLog"):
-            await log_channel.send(
-                "Hi there! This cog is running in an invalid state. "
-                "Inform the devs and bot owner that the unload loop is not setup properly."
-                "This will require restarting the bot after fixing the cog."
-            )
-            return
-
-        old_data = deepcopy(await self.config.clans())
-        try:
-            await self.refresh_data()
-        except Exception as e:
-            if debug_channel:
-                debug_channel.send(f"Error {e} when updating data.")
-            if self.last_error_time:
-                if self.last_error_time - time.time() >= 300:
-                    log.exception(f"Error {e} when updating data.", exc_info=e)
-                    self.last_error_time = time.time()
-            return
-
-        new_data = deepcopy(await self.config.clans())
-
-        if len(new_data) == 0:
-            log.error("Clans data is empty.")
-            return
-
-        if len(old_data) == 0:
-            await self.config.clans.set(list(new_data))
-            return
-
-        for index, clan in enumerate(new_data):
-            old_clan_data = {}
-            new_clan_data = {}
-            for member_data in old_data[index]["member_list"]:
-                old_clan_data[member_data["tag"]] = member_data["name"]
-            for member_data in clan["member_list"]:
-                new_clan_data[member_data["tag"]] = member_data["name"]
-            clan_tag = clan["tag"]
-
-            if len(old_clan_data) > 0:
-                total = set(list(new_clan_data.keys())).union(
-                    set(list(old_clan_data.keys()))
+            description = ""
+            for player_tag in players_left_clan:
+                player_name = old_members_data.get(player_tag, None) or "Unnamed Player"
+                sad_emote = self.bot.get_emoji(592001717311242241) or ""
+                description += "{}({}) has left {} {}".format(
+                    player_name, player_tag, data["name"], sad_emote
                 )
-                players_left_clan = list(total - set(new_clan_data.keys()))
-                players_joined_clan = list(total - set(old_clan_data.keys()))
-                # if len(players_left_clan) > 0 or len(players_joined_clan) > 0:
-                #     print("Total: ", total)
-                #     print("New: ", new_clan_data.keys())
-                #     print("Old: ", old_clan_data.keys())
-                description = ""
-                for player_tag in players_left_clan:
-                    player_name = old_clan_data.get(player_tag) or "wtf"
-                    sad_emote = self.bot.get_emoji(592001717311242241) or ""
-                    description += "{}({}) has left {} {}".format(
-                        player_name, player_tag, clan["name"], sad_emote
-                    )
-                if description:
-                    embed = discord.Embed(
-                        title="Member Left",
-                        description=description,
-                        colour=discord.Colour.blue(),
-                    )
-                    await log_channel.send(embed=embed)
-                description = ""
-                for player_tag in players_joined_clan:
-                    player_name = new_clan_data.get(player_tag) or "wtf"
-                    happy_emote = self.bot.get_emoji(375143193630605332) or ""
-                    description += "{}({}) has joined {} {}".format(
-                        player_name, player_tag, clan["name"], happy_emote
-                    )
-                if description:
-                    embed = discord.Embed(
-                        title="Member Joined",
-                        description=description,
-                        colour=discord.Colour.blue(),
-                    )
-                    await log_channel.send(embed=embed)
-
-        await self.config.clans.set(list(new_data))
-        self.last_error_time = None
-        self.last_updated = datetime.now()
-        self.last_updated_preety = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-        if debug_channel:
-            await debug_channel.send(
-                "Loop iteration {} has ended at {}.".format(
-                    self.loop_count, self.last_updated_preety
+            if description:
+                embed = discord.Embed(
+                    title="Member Left",
+                    description=description,
+                    colour=discord.Colour.blue(),
                 )
-            )
-        self.loop_count += 1
+                await log_channel.send(embed=embed)
+            description = ""
+            for player_tag in players_joined_clan:
+                player_name = new_members_data.get(player_tag) or "Unnamed Player"
+                happy_emote = self.bot.get_emoji(375143193630605332) or ""
+                description += "{}({}) has joined {} {}".format(
+                    player_name, player_tag, key, happy_emote
+                )
+            if description:
+                embed = discord.Embed(
+                    title="Member Joined",
+                    description=description,
+                    colour=discord.Colour.blue(),
+                )
+                await log_channel.send(embed=embed)
 
-    @commands.command()
-    @checks.admin_or_permissions()
-    async def stopclanlog(self, ctx):
-        if self.refresh_task:
-            self.refresh_task.stop()
+    @commands.group(name="clanlogset")
+    async def clanlogset(self, ctx):
+        pass
 
-    @commands.command()
-    @checks.admin_or_permissions()
-    async def setclanloglogchannel(self, ctx, channel: discord.TextChannel):
-        await self.config.log_channel.set(channel.id)
-        await ctx.send("Clanlog channel has been set to {}".format(channel.mention))
-        await ctx.tick()
-
-    @commands.command()
+    @clanlogset.command(name="global_log")
     @checks.is_owner()
-    async def setclanlogdebugchannel(self, ctx, channel: Optional[discord.TextChannel]):
+    async def clanlogset_global_log_channel(self, ctx, channel: Optional[discord.TextChannel] = None):
         if channel is None:
-            await self.config.debug_channel.set(None)
-            await ctx.send("Debug disabled.")
+            await self.config.global_log_channel.set(channel)
+            await ctx.send("Disabled global clanlog")
             await ctx.tick()
             return
-        await self.config.debug_channel.set(channel.id)
-        await ctx.send("Debug channel has been set to {}".format(channel.mention))
+        await self.config.global_log_channel.set(channel.id)
+        await ctx.send("Global clanlog channel has been set to {}".format(channel.mention))
         await ctx.tick()
-
-    @commands.command()
-    @checks.is_owner()
-    async def clearoldclanlog(self, ctx):
-        await self.config.clans.set(list())
-        await ctx.tick()
-
-    def cog_unload(self):
-        if self.refresh_task:
-            self.refresh_task.cancel()
-        if self.token_task:
-            self.token_task.cancel()
-        if self.clash:
-            close_task = self.bot.loop.create_task(self.clash.close())
