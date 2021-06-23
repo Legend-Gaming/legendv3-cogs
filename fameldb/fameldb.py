9c2a99ca2cd76483da4bdc7d486c44903137d204
kingslayer268 <36760455+kingslayer268@users.noreply.github.com>
2021-06-23 11:35:59 +0530

    Update fameldb.py

--- fameldb/fameldb.py
+++ fameldb/fameldb.py
@@ -273,6 +273,12 @@ class FameLeaderboard(commands.Cog):
         else:
             embed_dict = dict()
             for tag in clan_mem_dict:
+                embed = discord.Embed(title="Legend Clash Royale Fame Leaderboard",
+                                      description='These are the top fame contributors from Legend Clans in the current river race!', color=0x80ff00)
+                embed.set_thumbnail(
+                         url="https://static.wikia.nocookie.net/clashroyale/images/9/9f/War_Shield.png/revision/latest?cb=20180425130200")
+                embed.set_footer(text="Bot by: Legend Dev Team",
+                         icon_url="https://cdn.discordapp.com/emojis/709796075581735012.gif?v=1")
                 em = await self.ldb_to_emb(ldb=clan_mem_dict[tag], base_embed=copy.copy(embed), clan_spec=True)
                 embed_dict['tag'] = em
             return main_emb, embed_dict
